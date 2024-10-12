cbd9b7c8e6eacc8bc012010748cac6ff30a16d7a
Dr. Peter Netz <152088022+zentrocdot@users.noreply.github.com>
2024-10-12 12:29:54 +0200

    Update sd-webui-aspect_ratios-dd.py

--- scripts/sd-webui-aspect_ratios-dd.py
+++ scripts/sd-webui-aspect_ratios-dd.py
@@ -115,7 +115,6 @@ class AspectRatioScript(scripts.Script):
                 elem_id=css_acc
             ) as enabled:
                 arval = gr.Dropdown(arlist, label="Aspect Ratios", value="1:1")
-                exact = gr.Textbox(label="Calculate value is:")
                 with gr.Row(elem_id=css_row):
                     rst = AspectRatioButton(ar=1.0, value="Reset")
                     btn = AspectRatioButton(ar=1.0, value="Apply")
